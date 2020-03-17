3a8a532ab6c813d03d1e3980c6af541002792873
Cristian Andrade <cy.andrade@uniandes.edu.co>
2020-03-17 12:14:01 -0500

    fixed channels=N_C instead toGrayscale

--- data_processing.py
+++ data_processing.py
@@ -6,17 +6,16 @@ AUTOTUNE = tf.data.experimental.AUTOTUNE
 N_H = 64
 N_W = 64
 N_C = 1
+N_C_loaded = 1
 
 def load_process_fp_dataset(data_dir_patt,input_shape,batch_size):
     global N_H,N_W,N_C
 
     ds_data_dirs = tf.data.Dataset.list_files(data_dir_patt[0]+"/*"+data_dir_patt[1],shuffle=False)
 
-    '''
     data_list = list(ds_data_dirs.as_numpy_iterator())
     print("")
     print("Fingerprints Loaded:",len(data_list),"from:",data_dir_patt[0],"\n")
-    '''
 
     N_H = input_shape[0]
     N_W = input_shape[1]
@@ -28,23 +27,19 @@ def load_process_fp_dataset(data_dir_patt,input_shape,batch_size):
     ds_data_dirs = ds_data_dirs.batch(batch_size,True)
     ds_data_dirs = ds_data_dirs.prefetch(buffer_size=AUTOTUNE)
 
-    '''
     data_list_batched = list(ds_data_dirs.as_numpy_iterator())
     print("Batches Created:",len(data_list_batched))
     print("shape of batch:",data_list_batched[0].shape,"\n")
-    '''
 
     return ds_data_dirs
 
 def load_process_images(file_path):
+    global N_H,N_W,N_C
     img = tf.io.read_file(file_path)
-    img = tf.io.decode_png(img)
-    if N_C == 1:
-        img = tf.image.rgb_to_grayscale(img)
+    img = tf.io.decode_image(img,channels=N_C)
     img = tf.image.resize(img, [N_H, N_W],preserve_aspect_ratio=False)
     img = tf.cast(img,tf.float32)
     img = img*(2.0/255.0)-1.0
-
     return img
 
 def load_verification_images(fps_shape,num_fps):
