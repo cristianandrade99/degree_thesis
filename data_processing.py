2558f5995acb364f64ddcf169d0081d1b3cd23e2
Cristian Andrade <cy.andrade@uniandes.edu.co>
2020-03-24 12:25:55 -0500

    data augmentation fliping

--- data_processing.py
+++ data_processing.py
@@ -13,36 +13,30 @@ def load_process_fp_dataset(data_dir_patt,input_shape,batch_size):
     global N_H,N_W,N_C
 
     paterns = list_folder_patterns(data_dir_patt[0],data_dir_patt[1])
-    ds_data_dirs = tf.data.Dataset.list_files(paterns,shuffle=False)
+    ds_data_dirs_orig = tf.data.Dataset.list_files(paterns,shuffle=False)
 
-    '''data_list = list(ds_data_dirs.as_numpy_iterator())
+    data_list = list(ds_data_dirs_orig.as_numpy_iterator())
     print("")
-    print("Fingerprints Loaded:",len(data_list),"from:",data_dir_patt[0],"\n")'''
+    print("Fingerprints Loaded:",len(data_list),"from:",data_dir_patt[0],"\n")
 
     N_H = input_shape[0]
     N_W = input_shape[1]
     N_C = input_shape[2]
 
-    ds_data_dirs = ds_data_dirs.map(load_process_images, num_parallel_calls=AUTOTUNE)
+    ds_data_dirs_orig = ds_data_dirs_orig.map(read_orig_images, num_parallel_calls=AUTOTUNE)
+    ds_data_dirs_inv = ds_data_dirs_orig.map(convert_inv_images, num_parallel_calls=AUTOTUNE)
 
-    ds_data_dirs = ds_data_dirs.shuffle(buffer_size=128)
+    ds_data_dirs = ds_data_dirs_orig.concatenate(ds_data_dirs_inv)
+
+    #ds_data_dirs = ds_data_dirs.shuffle(buffer_size=128)
     ds_data_dirs = ds_data_dirs.batch(batch_size,True)
     ds_data_dirs = ds_data_dirs.prefetch(buffer_size=AUTOTUNE)
 
-    '''data_list_batched = list(ds_data_dirs.as_numpy_iterator())
+    data_list_batched = list(ds_data_dirs.as_numpy_iterator())
     print("Batches Created:",len(data_list_batched))
-    print("shape of batch:",data_list_batched[0].shape,"\n")'''
+    print("shape of batch:",data_list_batched[0].shape,"\n")
 
     return ds_data_dirs
-
-def load_process_images(file_path):
-    global N_H,N_W,N_C
-    img = tf.io.read_file(file_path)
-    img = tf.io.decode_png(img,channels=N_C)
-    img = tf.image.resize(img, [N_H, N_W],preserve_aspect_ratio=False)
-    img = tf.cast(img,tf.float32)
-    img = img*(2.0/255.0)-1.0
-    return img
 
 def load_verification_images(fps_shape,num_fps):
     global N_H,N_W,N_C
@@ -72,3 +66,15 @@ def list_folder_patterns(root_dir,patern):
                 patterns.append(act_patern)
                 continue
     return patterns
+
+def read_orig_images(file_path):
+    global N_H,N_W,N_C
+    img = tf.io.read_file(file_path)
+    img = tf.io.decode_png(img,channels=N_C)
+    img = tf.image.resize(img, [N_H, N_W],preserve_aspect_ratio=False)
+    img = tf.cast(img,tf.float32)
+    img = img*(2.0/255.0)-1.0
+    return img
+
+def convert_inv_images(img):
+    return tf.image.flip_left_right(img)
